98a653abb1290081ab238f60839da105f00c8d62
Gian Bernardo <gianszabernardo@gmail.com>
2017-12-10 20:14:42 -0200

    Base project creation.

---
+++ index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require( 'electron' );
+const path = require( 'path' );
+const url = require( 'url' );
+const config = require('./app/config.json');
+
+let appWindow;
+
+app.on ( 'ready', () => {
+
+    appWindow = new BrowserWindow({
+        height: config.window.height,
+        width: config.window.width,
+        maximizable: config.window.maximizable,
+        show: true
+    });
+
+    appWindow.loadURL( url.format({
+        pathname: path.join( __dirname, 'app/index.html' ),
+        protocol: 'file',
+        slashes: true,
+    }));
+
+    appWindow.on( 'ready-to-show', () => {
+        appWindow.show();
+    });
+
+    appWindow.on( 'close', () => {
+        appWindow = null;
+    });
+});

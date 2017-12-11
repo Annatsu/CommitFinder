859e02fadc353b68c6d7a3b3270b259bac68e7c7
Gian Bernardo <gianszabernardo@gmail.com>
2017-12-10 22:10:48 -0200

    Develop branch created.

--- app/assets/js/commit.js
+++ app/assets/js/commit.js
@@ -1,29 +1,29 @@
 $( document ).ready( function(){
-
+	
 	let nick = $( '#nick' );
 	let repo = $( '#repo' );
 	let commitNumber = $( '#commitNumber' );
 	let submitBtn = $( '#submit-btn' );
-
+			
 	let commit = {};
-
+			
 	function isValid(){
 		if ( nick.val().length < 1 || repo.val().length < 1 || commitNumber.val().length < 1 ) {
 			window.alert( 'Nick, Repositório or commits number invalid.' );
 			return false;
 		} else return true;
-	}
-
+	}			
+	
 	submitBtn.on({
 		'click': function(){
-
+			
 			nick.val().trim();
 			repo.val().trim();
-
+			
 			if ( isValid() ) {
 				$.get( `https://api.github.com/repos/${nick.val()}/${repo.val()}/commits`, ( data ) => {
 					data.reverse();
-
+			
 					if ( commitNumber.val() > data.length ){
 						alert( `Only exists ${data.length} commits on this repository.` );
 						return;
@@ -35,5 +35,5 @@ $( document ).ready( function(){
 				});
 			} else return;
 		}
-	})
+	});
 });
---
+++ app/main.js
@@ -0,0 +1,1 @@
+window.jquery = window.$ = require( 'jquery' );

d9dfd72bc5b36572b0a1a02a620bfcf145113fb9
TheD <gianszabernardo@gmail.com>
2018-05-29 22:46:09 -0300

    Update code to Vanilla JS (ECMA6)

--- app/assets/js/commit.js
+++ app/assets/js/commit.js
@@ -1,40 +1,50 @@
-$( document ).ready( function(){
+document.addEventListener('DOMContentLoaded', event => {
 
-	let nick = $( '#nick' );
-	let repo = $( '#repo' );
-	let commitNumber = $( '#commitNumber' );
-	let submitBtn = $( '#submit-btn' );
+    const commit = {};
 
-	let commit = {};
+	const nick = document.getElementById('nick');
+	const repo = document.getElementById('repo');
+	const commitNumber = document.getElementById('commitNumber');
+    const submitBtn = document.getElementById('submit-btn');
 
-	function isValid(){
-		if ( nick.val().length < 1 || repo.val().length < 1 || commitNumber.val().length < 1 ) {
-			window.alert( 'Nick, Repositório or commits number invalid.' );
-			return false;
-		} 
-		return true;
-	}
+    const trimValues = (...elements) => {
+        elements.forEach(element => {
+            element.value.trim();
+        });
+    };
+    
+    const isValid = () => {
+        nick.value.length < 1 ||
+        repo.value.length < 1 ||
+        commitNumber.value.length < 1 ?
+        false : true
+    };
 
-	submitBtn.on({
-		'click': function(){
+    submitBtn.onclick = () => {
+        trimValues(nick, repo);
+        if (isValid) {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if(this.readyState == 4 && this.status == 200) {
+                    const response = xhttp.responseText;
+                    const data = JSON.parse(response);
+    
+                    data.reverse();
 
-			nick.val().trim();
-			repo.val().trim();
-
-			if ( isValid() ) {
-				$.get( `https://api.github.com/repos/${nick.val()}/${repo.val()}/commits`, ( data ) => {
-					data.reverse();
-
-					if ( commitNumber.val() > data.length ){
-						alert( `Only exists ${data.length} commits on this repository.` );
-						return;
-					}
-					else {
-						commit = data[ commitNumber.val() - 1 ];
-						window.open( commit[ 'html_url' ] );
-					}
-				});
-			} else return;
-		}
-	})
+                    if (commitNumber.value > data.length){
+                        alert(`Only exists ${data.length} commits on this repository.`);
+                        return;
+                    } else {
+                        // commit = data[commitNumber.value - 1];
+                        // console.log(Object.assign(commit, data[commitNumber.value - 1]))
+                        Object.assign(commit, data[commitNumber.value - 1]);
+                        open(commit['html_url']);
+                    }
+                }
+            }
+            xhttp.open('GET', `https://api.github.com/repos/${nick.value}/${repo.value}/commits`, true);
+            xhttp.send();
+        } 
+        else return;
+    }
 });
--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const { app, BrowserWindow } = require( 'electron' );
-const path = require( 'path' );
-const url = require( 'url' );
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 const config = require('./app/config.json');
 
 let appWindow;
 
-app.on ( 'ready', () => {
+app.on('ready', () => {
 
     appWindow = new BrowserWindow({
         height: config.window.height,
         width: config.window.width,
-        maximizable: config.window.maximizable,
         show: true
     });
 
-    appWindow.loadURL( url.format({
-        pathname: path.join( __dirname, 'app/index.html' ),
+    appWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
         protocol: 'file',
         slashes: true,
     }));
 
-    appWindow.on( 'ready-to-show', () => {
+    appWindow.on('ready-to-show', () => {
         appWindow.show();
     });
 
-    appWindow.on( 'close', () => {
+    appWindow.on('close', () => {
         appWindow = null;
     });
 });

704033b5de77cff9b0e33fd220f69d2be920ab68
TheD <gianszabernardo@gmail.com>
2018-05-29 22:55:26 -0300

    Add some documentation

--- app/assets/js/commit.js
+++ app/assets/js/commit.js
@@ -1,18 +1,52 @@
+/**
+ * Document Ready Event.
+ */
 document.addEventListener('DOMContentLoaded', event => {
-
+    /**
+     * Object to hold commit data.
+     */
     const commit = {};
 
+    /**
+     * The user nickname.
+     */
 	const nick = document.getElementById('nick');
-	const repo = document.getElementById('repo');
-	const commitNumber = document.getElementById('commitNumber');
+    
+    /**
+     * The selected repository.
+     */
+    const repo = document.getElementById('repo');
+
+    /**
+     * The selected commit by a number.
+     */
+    const commitNumber = document.getElementById('commitNumber');
+    
+    /**
+     * The button to trigger 'onclick' event.
+     */
     const submitBtn = document.getElementById('submit-btn');
 
+    /**
+     * @function trimValues
+     * 
+     * Trim all selected elements.
+     * 
+     * @param {HTMLElement} elements The selected elements to be trimmed.
+     */
     const trimValues = (...elements) => {
         elements.forEach(element => {
             element.value.trim();
         });
     };
     
+    /**
+     * @function isValid
+     * 
+     * Check if the forms are all valid.
+     * 
+     * @returns {boolean} Returns if it's valid or not.
+     */
     const isValid = () => {
         nick.value.length < 1 ||
         repo.value.length < 1 ||
@@ -23,6 +57,7 @@ document.addEventListener('DOMContentLoaded', event => {
     submitBtn.onclick = () => {
         trimValues(nick, repo);
         if (isValid) {
+            // Create a new xhttp request.
             const xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if(this.readyState == 4 && this.status == 200) {
@@ -35,8 +70,6 @@ document.addEventListener('DOMContentLoaded', event => {
                         alert(`Only exists ${data.length} commits on this repository.`);
                         return;
                     } else {
-                        // commit = data[commitNumber.value - 1];
-                        // console.log(Object.assign(commit, data[commitNumber.value - 1]))
                         Object.assign(commit, data[commitNumber.value - 1]);
                         open(commit['html_url']);
                     }
